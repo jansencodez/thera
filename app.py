cd751f4fa9fbdf2f378e3ba8e4a7a7db6cde07d1
jansencodez <nyamwecliff@gmail.com>
2025-02-25 12:12:24 +0300

    up

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://172.25.176.1:3000"
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
